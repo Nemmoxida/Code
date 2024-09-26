40adb830259954273114dc38194b1cd89fffa390
Ilham Fauzy <ilham.kume02@gmail.com>
2024-09-26 20:56:57 +0700

    changed name
    from penjumlahan to aritmatika

--- Aritmatika.java
+++ Aritmatika.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-public class penjumlahan {
+public class Aritmatika {
     public static void main(String[] args) {
 
         // membuat scanner
